6f13612f2c40a8b05ec5d0fb17ad82b0276b784b
Ray Marceau <ray@raymarceau.com>
2021-09-02 16:28:34 -0700

    update webpack to 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -9,18 +9,18 @@ module.exports = {
     index: './src/content/index.js',
     index_purse: './src/content/index_purse.js',
     event: './src/event/index.js',
-    popup: './src/popup/index.js'
+    popup: './src/popup/index.js',
   },
 
   output: {
     filename: '[name].js',
     path: path.resolve('dist'),
-    publicPath: '/'
+    publicPath: '/',
   },
 
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.json'],
-    modules: ['node_modules']
+    modules: ['node_modules'],
   },
 
   module: {
@@ -28,31 +28,37 @@ module.exports = {
       {
         test: /\.js|jsx?$/,
         loader: 'babel-loader',
-        exclude: /(node_modules)/
+        exclude: /(node_modules)/,
       },
       {
         test: /\.css?$/,
-        use: [ 'style-loader', 'css-loader' ],
-        exclude: /(node_modules)/
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+        exclude: /(node_modules)/,
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([
-      'manifest.json',
-    {
-      from: 'src/popup/index.html',
-      to: 'popup.html'
-    },{
-      from: 'src/scripts',
-      to: 'scripts'
-    },{
-      from: 'src/styles',
-      to: 'styles'
-    },{
-      from: 'icons',
-      to: 'icons'
-    }])
-  ]
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        'manifest.json',
+        {
+          from: 'src/popup/index.html',
+          to: 'popup.html',
+        },
+        {
+          from: 'src/scripts',
+          to: 'scripts',
+        },
+        {
+          from: 'src/styles',
+          to: 'styles',
+        },
+        {
+          from: 'icons',
+          to: 'icons',
+        },
+      ],
+    }),
+  ],
 };
