aa4c1e855af0fa226c62af868de24d6f2e8ee76c
Ray Marceau <ray@raymarceau.com>
2021-09-03 13:59:02 -0700

    add persistent redux state for manifest v3

--- src/event/index.js
+++ src/event/index.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import storeCreatorFactory from 'reduxed-chrome-storage';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import { wrapStore, alias } from 'react-chrome-redux';
 import aliases from './aliases';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    alias(aliases),
-    thunkMiddleware
-  )
-);
+const options = {
+  createStore: createStore,
+};
+const asyncStoreCreator = storeCreatorFactory(options);
 
-wrapStore(store, {
-  portName: 'buywithbtc'
-});
+asyncStoreCreator(rootReducer, applyMiddleware(alias(aliases), thunkMiddleware))
+  .then((store) => {
+    wrapStore(store, {
+      portName: 'buywithbtc',
+    });
+  });
