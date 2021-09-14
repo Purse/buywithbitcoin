a1a57617b8b6eebb6a201e8f621b6706e8ab836d
Ray Marceau <ray@raymarceau.com>
2021-09-14 14:06:53 -0700

    remove deprecated babel-polyfill in favor of core-js.  use web-ext for compress-build (for better firefox integration)

--- src/event/actions/index.js
+++ src/event/actions/index.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 function fetchUpdateList(originalAction) {
   const { token, username, body } = originalAction;
